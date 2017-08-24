095f6742113bd3a46be0c7f1fa3773099f2625d1
greentea <741024920@qq.com>
2017-08-24 18:04:57 +0800

    分享朋友

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -69,6 +69,7 @@ export default {
         wx.config(this.config)
 
         /*s*/
+//        分享到朋友圈
         wx.onMenuShareTimeline({
           title: '游戏酱紫', // 分享标题
           link: 'http://h5.wan855.cn', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
@@ -96,6 +97,36 @@ export default {
           }
         })
         /*e*/
+
+//        分享给朋友
+        wx.onMenuShareAppMessage({
+          title: '游戏酱紫', // 分享标题
+          desc: '', // 分享描述
+          link: 'http://h5.wan855.cn', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: 'http://h5.wan855.cn/logo.png', // 分享图标
+          type: '', // 分享类型,music、video或link，不填默认为link
+          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+          success: function () {
+            // 用户确认分享后执行的回调函数
+            alert('分享成功!')
+            _this.$axios.get('/api/Integral/Task/share')
+              .then(res => {
+                if (res.code === '200'){
+                  console.log(res.msg)
+                  location.reload()
+                }
+              })
+              .catch(function(error){
+                console.log(error)
+              })
+          },
+          cancel: function () {
+            // 用户取消分享后执行的回调函数
+            alert('用户取消分享后执行的回调函数!')
+          }
+        });
+
+
 
       })
       .catch(function(error){
