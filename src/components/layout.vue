65cf951b616a3731ff4fbbecf0419e109756789f
greentea <741024920@qq.com>
2017-09-08 18:01:40 +0800

    修复微信及bug

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -108,7 +108,7 @@ export default {
           dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
           success: function () {
             // 用户确认分享后执行的回调函数
-            alert('分享成功!')
+//            alert('分享成功!')
             _this.$axios.get('/api/Integral/Task/share')
               .then(res => {
                 if (res.code === '200'){
@@ -122,7 +122,7 @@ export default {
           },
           cancel: function () {
             // 用户取消分享后执行的回调函数
-            alert('用户取消分享后执行的回调函数!')
+//            alert('用户取消分享后执行的回调函数!')
           }
         });
 
